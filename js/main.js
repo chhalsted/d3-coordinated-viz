c11dd8f023d64b758556dabcfcc1f7dde7df9cec
chhalsted <42559539+chhalsted@users.noreply.github.com>
2019-11-06 20:55:07 -0500

    Commit - Lab 2.3 Lesson 1 completed 11/6/19 9:00PM

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 //Christian Halsted 2019
 
+//First line of main.js...wrap everything in a self-executing anonymous function to move to local scope
+(function(){
+
+//pseudo-global variables
+var attrArray = ['sfhomepermits2010_17'
+                ,'totalhomevaluebuilt2010_17'
+                ,'avghomevalue'
+                ,'housingunits2010'
+                ,'housingunits2017'
+                ,'housingunitschange'
+                ,'housingunitschangepercent'
+                ,'population2010'
+                ,'population2017'
+                ,'populationchange'
+                ,'popchangepercent'
+                ,'distancetocoast'];
+var expressed = attrArray[0]; //initial attribute
+
 //begin script when window loads
 window.onload = setMap();
 
@@ -36,32 +54,17 @@ function setMap(){
     .await(callback);
 
   function callback(error, csvData, counties, towns){
-    //create graticule generator
-    var graticule = d3.geoGraticule()
-      .step([.5, .5])
+    //place graticule background on the map
+    setGraticule(map, path);
 
-    //create graticule background
-    var gratBackground = map.append("path")
-      .datum(graticule.outline()) //bind graticule background
-      .attr("class", "gratBackground") //assign class for styling
-      .attr("d", path) //project graticule
-
-    //create graticule lines
-    var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-      .data(graticule.lines()) //bind graticule lines to each element to be created
-      .enter() //create an element for each datum
-      .append("path") //append each element to the svg as a path element
-      .attr("class", "gratLines") //assign class for styling
-      .attr("d", path); //project graticule lines
-
-    console.log(error);
+    // console.log(error);
     console.log(csvData);
 
-    console.log(counties);
+    // console.log(counties);
     var maineCounties = topojson.feature(counties, counties.objects.counties)
-    console.log(maineCounties);
+    // console.log(maineCounties);
 
-    console.log(towns);
+    //console.log(towns);
     var maineTowns = topojson.feature(towns, towns.objects.searsmont_towns_10miles).features
     console.log(maineTowns);
 
@@ -71,16 +74,124 @@ function setMap(){
       .attr("class", "countries")
       .attr("d", path);
 
-    //add Searsmont adjacent towns to map
-    var regions = map.selectAll(".regions")
-      .data(maineTowns)
-      .enter()
-      .append("path")
-      .attr("class", function(d){
-          return "regions " + d.properties.town;
-      })
-      .attr("d", path);
+    maineTowns = joinData(maineTowns, csvData);
 
+    //create the color scale
+    var colorScale = makeColorScale(csvData);
 
-  };
+    //add enumeration units to the map
+    setEnumerationUnits(maineTowns, map, path, colorScale);
+
+    //place graticule lines on the top of the map
+    setGraticuleLines(map, path);
+
+  };  //end of callback
+};  //end of setMap
+
+function createGraticuleGenerator(map, path){
+  //create graticule generator
+  var graticule = d3.geoGraticule()
+    .step([.5, .5])
+  return graticule;
 };
+
+function setGraticule(map, path){
+  var graticule = createGraticuleGenerator(map, path);
+  //create graticule background
+  var gratBackground = map.append("path")
+    .datum(graticule.outline()) //bind graticule background
+    .attr("class", "gratBackground") //assign class for styling
+    .attr("d", path) //project graticule
+};
+
+function setGraticuleLines(map, path){
+  var graticule = createGraticuleGenerator(map, path);
+  //create graticule lines
+  var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+    .data(graticule.lines()) //bind graticule lines to each element to be created
+    .enter() //create an element for each datum
+    .append("path") //append each element to the svg as a path element
+    .attr("class", "gratLines") //assign class for styling
+    .attr("d", path); //project graticule lines
+};
+
+function joinData(maineTowns, csvData){
+    //loop through csv to assign each set of csv attribute values to geojson region
+    for (var i=0; i<csvData.length; i++){
+      var csvRegion = csvData[i]; //the current region
+      var csvKey = csvRegion.geocode; //the CSV primary key
+
+      //loop through geojson regions to find correct region
+      for (var a=0; a<maineTowns.length; a++){
+        var geojsonProps = maineTowns[a].properties; //the current region geojson properties
+        var geojsonKey = geojsonProps.geocode; //the geojson primary key
+        //where primary keys match, transfer csv data to geojson properties object
+        if (geojsonKey == csvKey){
+          //assign all attributes and values
+          attrArray.forEach(function(attr){
+            var val = parseFloat(csvRegion[attr]); //get csv attribute value
+            geojsonProps[attr] = val; //assign attribute and value to geojson properties
+          });
+        };
+      };
+    };
+    console.log(maineTowns);
+    return maineTowns;
+};
+
+function setEnumerationUnits(maineTowns, map, path, colorScale){
+  //add Searsmont adjacent towns to map
+  var regions = map.selectAll(".regions")
+    .data(maineTowns)
+    .enter()
+    .append("path")
+    .attr("class", function(d){
+        return "regions " + d.properties.town;
+    })
+    .attr("d", path)
+    .style("fill", function(d){
+        //return colorScale(d.properties[expressed]);
+        return choropleth(d.properties, colorScale);
+    });
+};
+
+//function to create color scale generator
+function makeColorScale(data){
+    var colorClasses = [
+        "#D4B9DA",
+        "#C994C7",
+        "#DF65B0",
+        "#DD1C77",
+        "#980043"
+    ];
+
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    //build array of all values of the expressed attribute
+    var domainArray = [];
+    for (var i=0; i<data.length; i++){
+        var val = parseFloat(data[i][expressed]);
+        domainArray.push(val);
+    };
+
+    //assign array of expressed values as scale domain
+    colorScale.domain(domainArray);
+
+    return colorScale;
+};
+
+//function to test for data value and return color
+function choropleth(props, colorScale){
+    //make sure attribute value is a number
+    var val = parseFloat(props[expressed]);
+    //if attribute value exists, assign a color; otherwise assign gray
+    if (typeof val == 'number' && !isNaN(val)){
+        return colorScale(val);
+    } else {
+        return "#CCC";
+    };
+};
+
+})(); //last line of main.js - self-executing anonymous function
